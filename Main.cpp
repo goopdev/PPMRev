931c244d375ff09f55390f84024656e72c05d6e8
goopdev <goopdeveloper@gmail.com>
2016-09-08 02:06:59 +0000

    Initial commit

---
+++ Main.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <fstream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char** argv){
+	std::cout << "hi";
+	// Initialization
+	std::string ppmName;
+
+	std::fstream configFile;
+	configFile.open("settings.txt");
+	while(!configFile.eof()){
+		std::getline(configFile, ppmName);
+		std::cout << ppmName;
+	}
+	configFile.close();	
+	// Image file
+	std::fstream ppm;
+	int WIDTH, HEIGHT, MAXCOLOUR;
+	int lineInd;
+	std::string currentLine;
+	std::vector<std::string> colourContents;
+	ppm.open(ppmName, std::fstream::out);
+
+	while(!ppm.eof()){
+		std::getline(ppm, currentLine);
+		lineInd++;
+		if(lineInd == 2) WIDTH = std::stoi(currentLine);
+		if(lineInd == 3) HEIGHT = std::stoi(currentLine);
+		if(lineInd == 4) MAXCOLOUR = std::stoi(currentLine);
+		if(lineInd >= 5){
+			colourContents.push_back(currentLine);
+		}
+	}	
+	std::string finalProduct = "P3\n" + std::to_string(WIDTH) + "\n" + std::to_string(HEIGHT) + "\n" + std::to_string(MAXCOLOUR) + "\n";
+	for(int i = 0; i < colourContents.size(); i++){
+		std::reverse(colourContents[i].begin(), colourContents[i].end());
+		finalProduct += colourContents[i] + '\n';
+	}
+	ppm << finalProduct;
+	ppm.close();
+}
